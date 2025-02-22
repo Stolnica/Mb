dba527cffd27ac9492c4f8aee3472532854f78fc
evgen <sara.drozg@gmail.com>
2025-02-22 17:45:29 +0100

    zoom

--- stolnicaweb.py
+++ stolnicaweb.py
@@ -118,7 +118,7 @@ st.markdown("""
 # Meta tag za zoomanje na mobilnih napravah
 st.markdown("""
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=3, user-scalable=yes">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes">
 </head>
 """, unsafe_allow_html=True)
 
